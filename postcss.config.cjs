b72f1a844cf9d3aaf22e3bac9cd45047f89a4672
WalterGrimes <bako.bota2009@gmail.com>
2025-10-25 01:19:49 +0600

    Fix: Update Tailwind CSS PostCSS plugin to use @tailwindcss/postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,10 @@
 // postcss.config.js
 
 /** @type {import('postcss-load-config').Config} */
-module.exports = { // <-- ИСПОЛЬЗУЙТЕ module.exports ВМЕСТО export default
+// postcss.config.cjs
+module.exports = {
   plugins: {
-    // Tailwind CSS должен быть первым
-    tailwindcss: {}, 
-    // Autoprefixer должен идти после Tailwind
-    autoprefixer: {},
+    '@tailwindcss/postcss': {}, // <--- FIXED LINE
+    'autoprefixer': {},
   },
 };
